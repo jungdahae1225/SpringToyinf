3df2ebb93646091a52eae4bffd113f4070b8f430
jungdahae1225 <jungdahae1225@gmail.com>
2022-03-12 01:56:31 +0900

    Domain entity

--- src/main/java/com/example/springtoyinf/SpringToyinfApplication.java
+++ src/main/java/com/example/springtoyinf/SpringToyinfApplication.java
@@ -3,6 +3,9 @@ package com.example.springtoyinf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 실전 스프링부트 jpa활용1 프로젝트
+ */
 @SpringBootApplication
 public class SpringToyinfApplication {
 
---
+++ src/main/java/com/example/springtoyinf/domain/Address.java
@@ -0,0 +1,14 @@
+package com.example.springtoyinf.domain;
+
+
+import lombok.Getter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+public class Address {
+    private String city;
+    private String street;
+    private String zipcode;
+}
