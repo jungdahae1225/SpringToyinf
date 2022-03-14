ad8405f1504e0551e6efcdc2610669e3b2077f68
jungdahae1225 <jungdahae1225@gmail.com>
2022-03-14 17:25:40 +0900

    Member Domain dev

---
+++ src/main/java/com/example/springtoyinf/service/MemberService.java
@@ -0,0 +1,49 @@
+package com.example.springtoyinf.service;
+
+import com.example.springtoyinf.domain.Member;
+import com.example.springtoyinf.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class MemberService {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    /**
+     * 회원가입
+     */
+    @Transactional //변경
+    public Long join(Member member) {
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers =
+                memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+    
+    /**
+     * 단 건 회원 조회
+     */
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
