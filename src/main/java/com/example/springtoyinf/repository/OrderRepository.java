9249b0f7bd54fe27819354c2508895a438c777ac
jungdahae1225 <jungdahae1225@gmail.com>
2022-03-15 13:30:32 +0900

    dev OrderService

---
+++ src/main/java/com/example/springtoyinf/repository/OrderRepository.java
@@ -0,0 +1,19 @@
+package com.example.springtoyinf.repository;
+
+import com.example.springtoyinf.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+    private final EntityManager em;
+    public void save(Order order) {
+        em.persist(order);
+    }
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+// public List<Order> findAll(OrderSearch orderSearch) { ... }
+}
